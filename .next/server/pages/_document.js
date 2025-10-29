e51d1a8f3ec8d3df247c832b3e8442d7184f105e
saurabh ojha <sourabhojha12@gmail.com>
2025-10-30 02:05:19 +0530

    update next build

---
+++ .next/server/pages/_document.js
@@ -0,0 +1,8 @@
+"use strict";(()=>{var e={};e.id=660,e.ids=[660],e.modules={9981:(e,r,t)=>{t.r(r),t.d(r,{default:()=>i});var s=t(997),a=t(6859);function i(){return(0,s.jsxs)(a.Html,{lang:"en",children:[s.jsx(a.Head,{}),(0,s.jsxs)("body",{children:[s.jsx("script",{dangerouslySetInnerHTML:{__html:`
+              try {
+                const theme = localStorage.getItem('theme');
+                if (theme === 'dark') {
+                  document.documentElement.classList.add('dark');
+                }
+              } catch (e) {}
+            `}}),s.jsx(a.Main,{}),s.jsx(a.NextScript,{})]})]})}},2785:e=>{e.exports=require("next/dist/compiled/next-server/pages.runtime.prod.js")},6689:e=>{e.exports=require("react")},997:e=>{e.exports=require("react/jsx-runtime")},1017:e=>{e.exports=require("path")}};var r=require("../webpack-runtime.js");r.C(e);var t=e=>r(r.s=e),s=r.X(0,[567,859],()=>t(9981));module.exports=s})();
